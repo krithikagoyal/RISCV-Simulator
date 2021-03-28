6a980f7a129c1da9b54ae644c2cccf7fe9246aa6
tarun-singla <2001singlatarun1810@gmail.com>
2021-03-28 21:41:07 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,30 +1,34 @@
-# The project is developed as part of Computer Architecture class
-# Project Name: Functional Simulator for subset of RISCV Processor
+"""
+The project is developed as part of Computer Architecture class.
+Project Name: Functional Simulator for subset of RISCV Processor
 
-# Developer's Name:
-# Developer's Email id:
-# Date: 
-
+-------------------------------------------------
+| Developer's Name   | Developer's Email ID     |
+|-----------------------------------------------|
+| Akhil Arya         | 2019csb1066@iitrpr.ac.in |
+| Harshwardhan Kumar | 2019csb1089@iitrpr.ac.in |
+| Krithika Goyal     | 2019csb1094@iitrpr.ac.in |
+| Rhythm Jain        | 2019csb1111@iitrpr.ac.in |
+| Tarun Singla       | 2019csb1126@iitrpr.ac.in |
+-------------------------------------------------
+"""
 
 # main.py
-# Purpose of this file: The file handles the input and output, and
-# invokes the simulator
-
+# Purpose of this file: The file handles the input and output, and invokes the simulator.
 
 from myRISCVSim import *
 import sys
 
 if __name__ == '__main__':
-    prog_mc_file = ''
     if len(sys.argv) < 2:
         print("Incorrect number of arguments. Please invoke the simulator \n\t./myRISCVSim <input mc file> \n")
         exit(1)
-    
-    #reset the processor
+
+    # reset the processor
     reset_proc()
 
-    #load the program memory
+    # load the program memory
     load_program_memory(sys.argv[1])
 
-    #run the simulator
+    # run the simulator
     run_RISCVsim()
