75871f3f2156386b60ba82ac7f23186e9e4f0260
Harshiitrpr <2019csb1089@iitrpr.ac.in>
2021-03-28 21:41:14 +0530

    debugged prog_mc_file

--- src/main.py
+++ src/main.py
@@ -15,16 +15,17 @@ from myRISCVSim import *
 import sys
 
 if __name__ == '__main__':
-    prog_mc_file = ''
+    
     if len(sys.argv) < 2:
         print("Incorrect number of arguments. Please invoke the simulator \n\t./myRISCVSim <input mc file> \n")
         exit(1)
     
+    prog_mc_file = sys.argv[1]
     #reset the processor
     reset_proc()
 
     #load the program memory
-    load_program_memory(sys.argv[1])
+    load_program_memory(prog_mc_file)
 
     #run the simulator
     run_RISCVsim()
