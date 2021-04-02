e6772bafc90ad085e811733abf6f93b17a315944
Harshwardhan Kumar <56429764+Harshiitrpr@users.noreply.github.com>
2021-04-02 21:50:29 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,14 +22,14 @@ import sys
 if __name__ == '__main__':
 
     # check for correct number of arguments
-    '''if len(sys.argv) < 2:
+    if len(sys.argv) < 2:
         print("Incorrect number of arguments. Please invoke the simulator \n\t./myRISCVSim <input mc file> \n")
         exit(1)
     
 
     # set .mc file
-    prog_mc_file = sys.argv[1]'''
-    prog_mc_file = 'input.mc'
+    prog_mc_file = sys.argv[1]
+    #prog_mc_file = 'input.mc'
 
     # reset the processor
     reset_proc()
