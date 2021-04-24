b3d9e8e5640808065b1890a684f574bf32494f37
Harshiitrpr <2019csb1089@iitrpr.ac.in>
2021-04-24 12:08:50 +0530

    ag

--- Phase 2/src/main.py
+++ Phase 2/src/main.py
@@ -20,25 +20,33 @@ from Gui import display, take_input
 from myRISCVSim import State, Processor, BTB, HDU
 import time
 
+stats = [
+	"Total number of cycles: ",
+	"Total instructions executed: ",
+	"CPI: ",
+	"Number of data-transfer(load and store): ",
+	"Number of ALU instructions executed: ",
+	"Number of Control instructions: ",
+	"Number of stalls/bubbles in the pipeline: ",
+	"Number of data hazards: ",
+	"Number of control hazards: ",
+	"Number of branch mispredictions: ",
+	"Number of stalls due to data hazards: ",
+	"Number of stalls due to control hazards: "
+]
+
+s = [0]*12
+
+# Function for pipelined execution
 def evaluate(processor, pipeline_ins):
 	processor.write_back(pipeline_ins[0])
-	if not pipeline_ins[0].is_dummy:
-		processor.s[1] += 1
-		if pipeline_ins[0].alu_control_signal in [16, 17, 18, 20, 21, 22]: #data transfer
-			processor.s[3] += 1
-		elif pipeline_ins[0].alu_control_signal in [19, 23,24,25,26, 29]:  #control instruction
-			processor.s[5] += 1
-		else:															   #ALU instruction
-			processor.s[4] += 1
-	else:
-		processor.s[6] += 1
-	
 	processor.mem(pipeline_ins[1])
 	processor.execute(pipeline_ins[2])
-	control_hazard, control_pc, state3 = processor.decode(pipeline_ins[3], btb)
+	control_hazard, control_pc = processor.decode(pipeline_ins[3], btb)
 	processor.fetch(pipeline_ins[4], btb)
 	pipeline_ins = [pipeline_ins[1], pipeline_ins[2], pipeline_ins[3], pipeline_ins[4]]
 	return pipeline_ins, control_hazard, control_pc
+
 
 if __name__ == '__main__':
 
@@ -51,9 +59,9 @@ if __name__ == '__main__':
 	btb = BTB()
 
 	# Knobs
-	pipelining_enabled = True                      # Knob1
-	forwarding_enabled = True                      # Knob2
-	print_registers_each_cycle = True              # Knob3
+	pipelining_enabled = True                    # Knob1
+	forwarding_enabled = False                      # Knob2
+	print_registers_each_cycle = False              # Knob3
 	print_pipeline_registers_and_cycle = False     # Knob4
 	print_specific_pipeline_register = [False, -1] # Knob5
 
@@ -70,32 +78,63 @@ if __name__ == '__main__':
 	total_number_of_stalls = 0
 	number_of_branch_mispredictions = 0
 
+
 	if not pipelining_enabled:
+		# Multi-cycle
 		processor.pipelining_enabled = False
 
 		while True:
 			instruction = State(PC)
+
 			processor.fetch(instruction)
-			processor.decode(instruction)
-			if processor.terminate:
-				prog_end = True
-				break
-			processor.execute(instruction)
-			if processor.terminate:
-				prog_end = True
-				break
-			processor.mem(instruction)
-			processor.write_back(instruction)
-
-			PC = processor.next_PC
-			clock_cycles += 5
-
+			clock_cycles += 1
 			if print_registers_each_cycle:
+				print("CLOCK CYCLE:", clock_cycles)
+				print("Register Data:")
 				for i in range(32):
-					print(processor.R[i], end=" ")
+					print("R" + str(i) + ":", processor.R[i], end=" ")
 				print("\n")
 
-			print(clock_cycles)
+			processor.decode(instruction)
+			clock_cycles += 1
+			if processor.terminate:
+				prog_end = True
+				break
+			if print_registers_each_cycle:
+				print("CLOCK CYCLE:", clock_cycles)
+				print("Register Data:")
+				for i in range(32):
+					print("R" + str(i) + ":", processor.R[i], end=" ")
+				print("\n")
+
+			processor.execute(instruction)
+			clock_cycles += 1
+			if print_registers_each_cycle:
+				print("CLOCK CYCLE:", clock_cycles)
+				print("Register Data:")
+				for i in range(32):
+					print("R" + str(i) + ":", processor.R[i], end=" ")
+				print("\n")
+
+			processor.mem(instruction)
+			clock_cycles += 1
+			if print_registers_each_cycle:
+				print("CLOCK CYCLE:", clock_cycles)
+				print("Register Data:")
+				for i in range(32):
+					print("R" + str(i) + ":", processor.R[i], end=" ")
+				print("\n")
+
+			processor.write_back(instruction)
+			clock_cycles += 1
+			if print_registers_each_cycle:
+				print("CLOCK CYCLE:", clock_cycles)
+				print("Register Data:")
+				for i in range(32):
+					print("R" + str(i) + ":", processor.R[i], end=" ")
+				print("\n")
+
+			PC = processor.next_PC
 
 	else:
 		processor.pipelining_enabled = True
@@ -108,9 +147,6 @@ if __name__ == '__main__':
 			if not forwarding_enabled:
 				data_hazard = hdu.data_hazard_stalling(pipeline_instructions)
 
-				# for x in pipeline_instructions:
-					# print("x.pcp = ", x.PC, x.is_dummy)
-
 				old_states = pipeline_instructions
 				pipeline_instructions, control_hazard, control_pc = evaluate(processor, pipeline_instructions)
 
@@ -119,44 +155,37 @@ if __name__ == '__main__':
 
 				PC += 4
 
-				if branch_taken and not data_hazard[0]:
+				if branch_taken and not data_hazard:
 					PC = branch_pc
-					# print("branch_pc", branch_pc)
 
-				if control_hazard and not data_hazard[0]:
+				if control_hazard and not data_hazard:
 					number_of_control_hazards += 1
 					number_of_stalls_due_to_control_hazards += 1
 					PC = control_pc
-					# print("control_pc = ", control_pc)
 					pipeline_instructions.append(State(PC))
 					pipeline_instructions[-2].is_dummy = True
 
-				if data_hazard[0]:
+				if data_hazard:
 					number_of_data_hazards += 1
 					number_of_stalls_due_to_data_hazards += 1
 					pipeline_instructions = pipeline_instructions[:2] + [State(0)] + old_states[3:]
 					pipeline_instructions[2].is_dummy = True
 					PC -= 4
 
-				# print("lll ", control_hazard, data_hazard, PC)
-				if not control_hazard and not data_hazard[0]:
+				if not control_hazard and not data_hazard:
 					pipeline_instructions.append(State(PC))
 
 				pipeline_instructions[-2].next_pc = PC
-				# print("len ", len(pipeline_instructions))
+
 				prog_end = True
 				for i in range(4):
 					x = pipeline_instructions[i]
-					# print("X.pc ", x.PC)
 					if not x.is_dummy:
 						prog_end = False
 						break
 
 			else:
 				data_hazard, if_stall, stall_position, pipeline_instructions = hdu.data_hazard_forwarding(pipeline_instructions)
-				# print("data_hazard = ", data_hazard)
-				# for x in pipeline_instructions:
-					# print("x.pcp = ", x.PC, x.is_dummy)
 
 				old_states = pipeline_instructions
 				pipeline_instructions, control_hazard, control_pc = evaluate(processor, pipeline_instructions)
@@ -168,13 +197,11 @@ if __name__ == '__main__':
 
 				if branch_taken and not if_stall:
 					PC = branch_pc
-					# print("branch_pc", branch_pc)
 
 				if control_hazard and not if_stall:
 					number_of_control_hazards += 1
 					number_of_stalls_due_to_control_hazards += 1
 					PC = control_pc
-					# print("control_pc = ", control_pc)
 					pipeline_instructions.append(State(PC))
 					pipeline_instructions[-2].is_dummy = True
 
@@ -182,18 +209,20 @@ if __name__ == '__main__':
 					number_of_stalls_due_to_data_hazards += 1
 
 					if stall_position == 0:
-						# print("stall")
 						pipeline_instructions = pipeline_instructions[:1] + [State(0)] + old_states[2:]
 						pipeline_instructions[1].is_dummy = True
 						PC -= 4
+
 					elif stall_position == 1:
 						pipeline_instructions = pipeline_instructions[:2] + [State(0)] + old_states[3:]
 						pipeline_instructions[2].is_dummy = True
 						PC -= 4
+
 					elif stall_position == 2 and not control_hazard:
 						pipeline_instructions = pipeline_instructions[:3] + [State(0)] + old_states[4:]
 						pipeline_instructions[3].is_dummy = True
 						PC -= 4
+
 					else:
 						number_of_control_hazards += 1
 						number_of_stalls_due_to_control_hazards += 1
@@ -203,7 +232,6 @@ if __name__ == '__main__':
 
 				number_of_data_hazards += data_hazard
 
-				# print("lll ", control_hazard, data_hazard, PC)
 				if not control_hazard and not if_stall:
 					pipeline_instructions.append(State(PC))
 
@@ -213,54 +241,45 @@ if __name__ == '__main__':
 					inst.decode_forwarding_op1 = False
 					inst.decode_forwarding_op2 = False
 
-				# print("len ", len(pipeline_instructions))
 				prog_end = True
 				for i in range(4):
 					x = pipeline_instructions[i]
-					# print("X.pc ", x.PC)
 					if not x.is_dummy:
 						prog_end = False
 						break
 
 			clock_cycles += 1
-			# if clock_cycles > 0:
-			# 	break
-
-
-			# if print_registers_each_cycle:
-			# 	for i in range(32):
-			# 		print(processor.R[i], end=" ")
-			# print("\n")
+			if print_registers_each_cycle:
+				print("CLOCK CYCLE:", clock_cycles)
+				print("Register Data:")
+				for i in range(32):
+					print("R" + str(i) + ":", processor.R[i], end=" ")
+				print("\n")
 
 			# Print specific pipeline register
-			# Shift this above among instructions or elsewhere
-			# if print_specific_pipeline_register[0]:
-			# 	pass
+			if print_specific_pipeline_register[0]:
+				if not print_registers_each_cycle:
+					print("CLOCK CYCLE:", clock_cycles)
 
 			# Print pipeline registers and cycle
-			# if print_pipeline_registers_and_cycle:
-			# pass
+			elif print_pipeline_registers_and_cycle and not print_registers_each_cycle:
+				print("CLOCK CYCLE:", clock_cycles)
 
-			# print(clock_cycles)
-			# print("PC = ", PC)
 
-	# Print Messages
-	processor.s[0] = clock_cycles
-	processor.s[1] = processor.s[1]
-	processor.s[2] = processor.s[0]/processor.s[1]
-	processor.s[3] = processor.s[3]
-	processor.s[4] = processor.s[4]
-	processor.s[5] = processor.s[5]
-	processor.s[6] = processor.s[6]
+	# Print Statistics
+	s[0] = clock_cycles
+	s[1] = processor.count_total_inst
+	s[2] = s[0]/s[1]
+	s[3] = processor.count_mem_inst
+	s[4] = processor.count_alu_inst
+	s[5] = processor.count_control_inst
 	if prog_end:
 		processor.write_data_memory()
 		for i in range(12):
-			processor.stats[i] += str(processor.s[i])
+			stats[i] += str(s[i]) + '\n'
 		statfile = open("stats.txt", "w")
-		statfile.writelines(processor.stats)
+		statfile.writelines(stats)
 		statfile.close()
-		display()
-
-
-# Redundant stages and all dummy maybe
-# use of second argument of data_hazard_stalling?
+		# this list is just for testing, original will be created by Harsh
+		l = [['decode', 'execute', 'mem', 'fetch', 'wb'], ['decode', 'execute', 'mem', 'fetch', 'wb'], ['decode', 'execute', 'mem', 'fetch', 'wb'], ['decode', 'execute', 'mem', 'fetch', 'wb']]
+		display(l)
