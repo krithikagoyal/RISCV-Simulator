620043d3c14c1055604be96b5852ec2193d9ba16
Harshiitrpr <2019csb1089@iitrpr.ac.in>
2021-05-06 04:58:28 +0530

    passing color coding data

--- Phase 3/src/main.py
+++ Phase 3/src/main.py
@@ -456,4 +456,5 @@ if __name__ == '__main__':
 			mem_gui.append(tmp)
 
 		# control_hazard_signals is a list on integers 0=> nothing; 1=> red ; 2 => yellow; 3=> green
+		#mem_gui is list of list of 3 elemetnts [fetch message, mem message, [1,0]] 1=>hit 0=> miss
 		display(l, control_hazard_signals, l_dash)
